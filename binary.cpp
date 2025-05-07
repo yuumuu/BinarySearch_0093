0536242ae9476fd8e7d6a330e2ad3f0d4a945b86
yuumuu <haidar.alhazen93@gmail.com>
2025-05-07 15:49:51 +0700

    Adding Display Logic for Sorted Elements - binary.cpp

--- binary.cpp
+++ binary.cpp
@@ -43,3 +43,18 @@ void bubbleSortArray() {
         pass++;
     } while (pass <= nPanjang - 1);
 }
+
+void display() {
+    cout << "\n======================================================\n";
+    cout << "\t\t\tElemen array setelah diurutkan";
+    cout << "\n======================================================\n";
+
+    for (int j = 0; j < nPanjang; j++) {
+        cout << element[j];
+        if (j < nPanjang - 1) {
+            cout << " --> ";
+        }
+    }
+
+    cout << endl;
+}
