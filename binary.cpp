8b38e37cec344079515a99812f6bae1e261f30f2
yuumuu <haidar.alhazen93@gmail.com>
2025-05-07 15:46:59 +0700

    Adding Input Logic - binary.cpp

--- binary.cpp
+++ binary.cpp
@@ -5,7 +5,24 @@ int element[10];
 int nPanjang;
 int x;
 
-int main() {
+void input() {
+    while (true) {
+        cout << "Masukkan banyaknya elemen pada array (maksimal 10): ";
+        cin >> nPanjang;
+        
+        if (nPanjang > 0 && nPanjang <= 10) {
+            break;
+        } else {
+            cout << "\n[!] Jumlah elemen tidak boleh lebih dari 10. Silahkan coba lagi.\n";
+        }
+    }
+    
+    cout << "\n======================================================\n";
+    cout << "\t\t\tMasukkan elemen array";
+    cout << "\n======================================================\n";
 
-    return 0;
+    for (int i = 0; i < nPanjang; i++) {
+        cout << "Elemen ke-" << i + 1 << " = ";
+        cin >> element[i];
+    }
 }
