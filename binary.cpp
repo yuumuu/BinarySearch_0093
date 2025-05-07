36bcded2373dc686985975eb7a7bfb6024104be6
yuumuu <haidar.alhazen93@gmail.com>
2025-05-07 15:54:39 +0700

    Adding Main Core Logic on binarySearch Procedure - binary.cpp

--- binary.cpp
+++ binary.cpp
@@ -69,5 +69,17 @@ void binarySearch() {
 
         cout << "Masukkan elemen yang ingin dicari: ";
         cin >> x;
+
+        int low = 0;
+        int high = nPanjang - 1;
+
+        do {
+            int mid = (low + high) / 2;
+            if (element[mid] == x)
+                cout << "\n[✓] Elemen " << x << " ditemukan pada indeks " << mid << ".\n";
+                return;
+            
+            if (x < element[mid]) {high = mid - 1;} else {low = mid + 1;}
+        } while (low <= high);
     } while (ulang == 'y' || ulang == 'Y');
 }
